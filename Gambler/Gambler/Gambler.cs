7bcbf2360d50771ac8a88ec8d132d345f0a20551
Your UserName <aratinaik151997@gmail.com>
2021-12-22 09:10:22 +0530

    Start the game with bet 1 at a time

---
+++ Gambler/Gambler/Gambler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambler
+{
+    class Gambler
+    {
+        int amount = 100;
+        int bet = 0;;
+        public void StartBet()
+        {
+            while (amount > 0 && amount < 100)
+            {
+                bet++;
+                amount = amount + bet;
+            }
+        }
+    }
+}
---
+++ Gambler/Gambler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Gambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Gambler gambler = new Gambler();
+            gambler.StartBet();
+        }
+    }
+}
